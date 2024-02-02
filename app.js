c8ce69d55a88aa1303b60e4007b7ddff5019ead7
Bouba <122236288+Boubacarkonate@users.noreply.github.com>
2024-02-02 09:47:44 +0100

    Update app.js

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ function startPeer(initiator) {
                 trickle: false,
                 config: {
                     iceServers: [
-                        { urls: 'stun:stun.stunprotocol.org:3478' }
+                        { urls: 'stun:stun.l.google.com:19302'' }
                     ]
                 }
             });
